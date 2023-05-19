10cafdbcf116b50bc2e5ff8f1dff2ca773b52287
Flávio Dutra <flaviomdutra@outlook.com>
2023-05-19 16:54:54 -0300

    feat: edit answer use case

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -17,6 +17,12 @@ export class InMemoryAnswersRepository {
     this.items.push(answer)
   }
 
+  async save(answer: Answer) {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items[itemIndex] = answer
+  }
+
   async delete(answer: Answer) {
     const itemIndex = this.items.findIndex((item) => item.id === answer.id)
 
